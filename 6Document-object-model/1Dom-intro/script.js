4705e4242e53abac891570781180dae7929d36c3
saikumar <saikumar@digisprint.com>
2023-08-06 21:13:33 +0530

    added document-object-model

---
+++ 6Document-object-model/1Dom-intro/script.js
@@ -0,0 +1,12 @@
+// console.log(window);
+
+// console.log(window.document);
+
+// console.dir(window.document);
+// console.log(document.body.innerHTML); 
+// console.log(document.links[0]);
+// console.log(document.getElementById('main'));
+// const main=document.getElementById('main');
+// main.innerHTML= '<h1>hey im learning javascript</h1>'
+
+document.querySelector('#main h1').innerText = "hello"
